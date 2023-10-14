9567b794b3ad42d8aba4e1b1ef94dca613f44944
HOANG HIEU <hoanghieubk1995@gmail.com>
2023-10-14 21:45:21 +0900

    update

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -27,32 +27,13 @@ const DocumentViewer = React.memo(({ uri }: { uri: string }) => {
   }, []);
 
   return (
-    <div
-      className="pdf"
-      style={{
-        height: "98vh",
-        width: "100%",
-        overflow: "hidden",
-        pointerEvents: isDraggingShape ? "none" : "auto",
-      }}
-    >
-      <DocViewer
-        documents={[
-          {
-            uri: uri,
-            fileType: uri
-              .substring(uri.length - 6)
-              .split(".")
-              .pop(),
-            fileName:
-              "remote " +
-              uri
-                .substring(uri.length - 6)
-                .split(".")
-                .pop() +
-              " file",
-          },
-        ]}
+    <div className="pdf" style={{ height: "98vh", width: "100%", overflow: 'hidden', pointerEvents: isDraggingShape ? 'none' : 'auto' }}>
+      <DocViewer 
+        documents={[{ 
+          uri: uri,
+          fileType: uri.substring(uri.length - 6).split('.').pop(),
+          fileName: "remote " + uri.substring(uri.length - 6).split('.').pop() + " file"
+        }]} 
         pluginRenderers={DocViewerRenderers}
         theme={{
           primary: "#5296d8",
@@ -61,6 +42,7 @@ const DocumentViewer = React.memo(({ uri }: { uri: string }) => {
           textPrimary: "#ffffff",
           textSecondary: "#5296d8",
           textTertiary: "#00000099",
+          disableThemeScrollbar: true,
         }}
       />
     </div>
@@ -68,31 +50,24 @@ const DocumentViewer = React.memo(({ uri }: { uri: string }) => {
 });
 
 export const Canvas = () => {
-  const {
-    canvasItems: initialCanvasItems,
-    addItemToCanvas,
-    undoCanvasAction,
-    uri,
-  } = useDraggable();
+  const { canvasItems: initialCanvasItems, addItemToCanvas, undoCanvasAction, uri } = useDraggable();
   const [canvasItems, setCanvasItems] = useState(initialCanvasItems);
   const [selectedShapeId, setSelectedShapeId] = useState<number | null>(null);
 
-  const handleDrop = useCallback(
-    (e: React.DragEvent<HTMLDivElement>) => {
-      e.preventDefault();
-      const type = e.dataTransfer.getData("type") as ShapeType;
-      const source = e.dataTransfer.getData("source");
-      const canvasRect = e.currentTarget.getBoundingClientRect();
-      const position = {
-        x: e.clientX - canvasRect.left,
-        y: e.clientY - canvasRect.top,
-      };
-      if (source === "library") {
-        addItemToCanvas({ type, id: Date.now(), isSelected: false }, position);
-      }
-    },
-    [addItemToCanvas]
-  );
+
+  const handleDrop = useCallback((e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const type = e.dataTransfer.getData("type") as ShapeType;
+    const source = e.dataTransfer.getData("source");
+    const canvasRect = e.currentTarget.getBoundingClientRect();
+    const position = {
+      x: e.clientX - canvasRect.left,
+      y: e.clientY - canvasRect.top,
+    };
+    if (source === "library") {
+      addItemToCanvas({ type, id: Date.now(), isSelected: false }, position);
+    }
+  }, [addItemToCanvas]);
 
   const handleDragOver = useCallback((e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
@@ -101,6 +76,9 @@ export const Canvas = () => {
   useEffect(() => {
     setCanvasItems(initialCanvasItems);
   }, [initialCanvasItems]);
+
+
+
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -134,24 +112,11 @@ export const Canvas = () => {
           key={item.id}
           position={{ x: item.position.x, y: item.position.y }}
           onStop={(e, data) => {
-            setCanvasItems((prevItems) =>
-              prevItems.map((i) =>
-                i.id === item.id
-                  ? {
-                      ...i,
-                      position: { x: data.x, y: data.y },
-                    }
-                  : i
-              )
-            );
+            setCanvasItems((prevItems) => prevItems.map((i) => i.id === item.id ? { ...i, position: { x: data.x, y: data.y } } : i));
           }}
         >
           <div
-            style={{
-              position: "fixed",
-              top: item.position.y,
-              left: item.position.x,
-            }}
+            style={{ position: "fixed", top: item.position.y, left: item.position.x }}
             onClick={(e) => {
               e.stopPropagation();
               setSelectedShapeId(item.id);
